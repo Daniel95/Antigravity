4cda426e0ab338e354c1b6996b0040ee7361768d
Daniel95 <idaniel95@me.com>
2017-01-26 00:01:54 +0100

    edited camera, added camera for levelselect

---
+++ Assets/Scripts/Camera/DragCamera.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float dragSpeed = 4.6f;
+
+    [SerializeField]
+    private float velocityDivider = 1.3f;
+
+    private Vector2 _velocity;
+
+    private Vector2 _lastMousePos;
+
+    private BoundsCamera _boundsCamera;
+
+    private float zStartPos;
+
+    private void Start()
+    {
+        _boundsCamera = GetComponent<BoundsCamera>();
+        zStartPos = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _lastMousePos = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 _offset = -Camera.main.ScreenToViewportPoint((Vector2)Input.mousePosition - _lastMousePos);
+            _velocity += _offset * dragSpeed;
+
+            _lastMousePos = Input.mousePosition;
+        }
+
+        transform.position = _boundsCamera.GetBoundsPosition((Vector2)transform.position + _velocity);
+        transform.position = new Vector3(transform.position.x, transform.position.y, zStartPos);
+        _velocity /= velocityDivider;
+    }
+}
---
+++ Assets/Scripts/Camera/SmoothFollowCamera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float smoothness = 0.375f;
+
+    private float _yStartPos;
+
+    private Vector2 _velocity;
+
+    private BoundsCamera boundsCamera;
+
+    void Start()
+    {
+        boundsCamera = GetComponent<BoundsCamera>();
+
+        _yStartPos = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 delta = target.position - transform.position;
+        Vector2 destination = (Vector2)transform.position + delta;
+        Vector2 nextPos = Vector2.SmoothDamp(transform.position, boundsCamera.GetBoundsPosition(destination), ref _velocity, smoothness, Mathf.Infinity, Time.deltaTime); 
+        transform.position = new Vector3(nextPos.x, nextPos.y, _yStartPos);
+    }
+}
