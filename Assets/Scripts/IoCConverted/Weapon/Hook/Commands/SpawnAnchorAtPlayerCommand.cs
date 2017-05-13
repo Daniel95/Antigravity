c3168898e49093b04b0f6a27bda610e1672830d7
Daniel95 <idaniel95@me.com>
2017-05-13 14:40:43 +0200

    stop tracking prefabs

--- Assets/Scripts/IoCConverted/Weapon/Hook/Commands/SpawnAnchorAtPlayerCommand.cs
+++ Assets/Scripts/IoCConverted/Weapon/Hook/Commands/SpawnAnchorAtPlayerCommand.cs
@@ -9,9 +9,6 @@ public class SpawnAnchorAtPlayerCommand : Command {
     [Inject] private Ref<IHookProjectile> hookProjectileRef;
 
     protected override void Execute() {
-        Debug.Log(hookProjectileRef.Get());
-        Debug.Log(hookProjectileRef.Get().Transform);
-
         hookRef.Get().AddAnchor(
             playerModel.Player.transform.position, 
             hookProjectileRef.Get().Transform
