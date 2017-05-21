dbf01f48a2a4bea13ec3bbc93e319eed1586091c
Daniel95 <idaniel95@me.com>
2017-05-21 15:04:08 +0200

    added grapplecontext & pullback context. added debug log collision commands

---
+++ Assets/Scripts/New/Commands/Debug/DebugLogCollision2DCommand.cs
@@ -0,0 +1,11 @@
+﻿using IoCPlus;
+using UnityEngine;
+
+public class DebugLogCollision2DCommand : Command {
+
+    [InjectParameter] private Collision2D collision;
+
+    protected override void Execute() {
+        Debug.Log("Collision with: " + collision.transform.name);
+    }
+}
