66aba5a93b8584040239de4dd749758f7481c9d6
Daniel95 <idaniel95@me.com>
2017-05-28 17:09:45 +0200

    added clear console log command. updated todo.

---
+++ Assets/Scripts/Commands/Debug/DebugClearLogCommand.cs
@@ -0,0 +1,12 @@
+﻿using IoCPlus;
+using System;
+using System.Reflection;
+
+public class DebugClearLogCommand : Command {
+
+    protected override void Execute() {
+        Type logEntries = Type.GetType("UnityEditorInternal.LogEntries,UnityEditor.dll");
+        MethodInfo clearMethod = logEntries.GetMethod("Clear", BindingFlags.Static | BindingFlags.Public);
+        clearMethod.Invoke(null, null);
+    }
+}
