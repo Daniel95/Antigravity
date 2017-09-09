26c73a8a0210f405cbe50ada4b830f18661f06ea
Daniel95 <idaniel95@me.com>
2017-09-09 18:51:34 +0200

    started method to merge and save tiles

--- Assets/Scripts/Game/States/LevelEditor/States/Edit/Commands/CombineTilesToPrefabAndSaveCommand.cs
+++ Assets/Scripts/Game/States/LevelEditor/States/Edit/Commands/CombineTilesToPrefabAndSaveCommand.cs
@@ -1,9 +1,42 @@
 ﻿using IoCPlus;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class CombineTilesToPrefabAndSaveCommand : Command {
 
+    private const string LEVEL_PREFAB_PATH = "LevelEditor/LevelPrefab";
+
     protected override void Execute() {
+        GameObject levelPrefab = Resources.Load<GameObject>(LEVEL_PREFAB_PATH);
+        GameObject levelGameObject = Object.Instantiate(levelPrefab);
 
+        Material material = levelGameObject.GetComponent<Material>();
+        SpriteRenderer spriteRenderer = levelGameObject.GetComponent<SpriteRenderer>();
+        CompositeCollider2D compositeCollider = levelGameObject.GetComponent<CompositeCollider2D>();
+
+        Dictionary<Vector2, Tile> grid = TileGrid.Grid;
+
+        List<GameObject> gameObjectsToCombine = new List<GameObject>();
+        foreach (Tile tile in grid.Values) {
+            GameObject originalTileGameObject = tile.GameObject;
+            GameObject duplicateTileGameObject = Object.Instantiate(originalTileGameObject);
+            Object.Destroy(originalTileGameObject);
+            gameObjectsToCombine.Add(duplicateTileGameObject);
+        }
+
+        List<SpriteRenderer> spriteRenderersToCombine = new List<SpriteRenderer>();
+        List<BoxCollider2D> boxCollidersToCombine = new List<BoxCollider2D>();
+
+        foreach (GameObject gameObjectToCombine in gameObjectsToCombine) {
+            spriteRenderersToCombine.Add(gameObjectToCombine.GetComponent<SpriteRenderer>());
+            boxCollidersToCombine.Add(gameObjectToCombine.GetComponent<BoxCollider2D>());
+        }
+
+        SpriteHelper.CombineSpritesOfGameObjects(spriteRenderersToCombine, material, spriteRenderer);
+        ColliderHelper.CombineBoxCollidersInCompositeCollider(boxCollidersToCombine, compositeCollider);
+
+        foreach (GameObject gameObjectToCombine in gameObjectsToCombine) {
+            Object.Destroy(gameObjectToCombine);
+        }
     }
-
 }
--- Assets/Scripts/Game/States/LevelEditor/States/Edit/Commands/SerializeTileGridCommand.cs
+++ Assets/Scripts/Game/States/LevelEditor/States/Edit/Commands/SerializeTileGridCommand.cs
@@ -6,7 +6,6 @@ public class SerializeTileGridCommand : Command {
 
     protected override void Execute() {
         Dictionary<Vector2, Tile> grid = TileGrid.Grid;
-        SerializeHelper
     }
 
 }
