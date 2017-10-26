a808085f30aed05732177af414292481de473907
Daniel95 <idaniel95@me.com>
2017-10-26 22:45:29 +0200

    changed movedirection parameter of turn to next direction

--- Assets/Scripts/Player/PlayerCharacter/PlayerTurnDirection/Commands/PlayerTurnToNextDirectionCommand.cs
+++ Assets/Scripts/Player/PlayerCharacter/PlayerTurnDirection/Commands/PlayerTurnToNextDirectionCommand.cs
@@ -9,14 +9,7 @@ public class PlayerTurnToNextDirectionCommand : Command {
     [Inject(Label.Player)] private Ref<ICharacterCollisionDirection> playerCollisionDirectionRef;
 
     protected override void Execute() {
-        Vector2 moveDirection;
-        Vector2 ceilPreviousVelocityDirection = playerVelocityRef.Get().GetCeilPreviousVelocityDirection();
-        if(ceilPreviousVelocityDirection != Vector2.zero) {
-            moveDirection = ceilPreviousVelocityDirection;
-        } else {
-            moveDirection = playerVelocityRef.Get().MoveDirection;
-        }
-
+        Vector2 moveDirection = playerVelocityRef.Get().MoveDirection;
         Vector2 collisionDirection = playerCollisionDirectionRef.Get().CollisionDirection;
         RaycastData combinedRaycastData = playerRaycastDirectionRef.Get().GetCombinedDirectionAndCenterDistances();
         Vector2 surroundingsDirection = SurroundingDirectionHelper.GetSurroundingsDirection(collisionDirection, combinedRaycastData.Direction);
