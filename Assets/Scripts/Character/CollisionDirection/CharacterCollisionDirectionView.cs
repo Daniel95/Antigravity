39b2b5718025c4c45ccff643599c09b42e7c9e71
Daniel95 <idaniel95@me.com>
2017-10-28 11:51:05 +0200

    fixed some issues in collision direction view

--- Assets/Scripts/Character/CollisionDirection/CharacterCollisionDirectionView.cs
+++ Assets/Scripts/Character/CollisionDirection/CharacterCollisionDirectionView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class CharacterCollisionDirectionView : View, ICharacterCollisionDirection {
 
     [Inject] private CharacterCollisionWithNewDirectionEvent characterCollisionWithNewDirectionEvent;
@@ -10,7 +11,7 @@ public class CharacterCollisionDirectionView : View, ICharacterCollisionDirectio
     public Vector2 CollisionDirection { get { return collisionDirection; } }
     public int SavedCollisionsCount { get { return collisions.Count; } }
 
-    private Dictionary<Collision2D, Vector2> collisions = new Dictionary<Collision2D, Vector2>();
+    private Dictionary<Collider2D, Vector2> collisions = new Dictionary<Collider2D, Vector2>();
     private Vector2 collisionDirection;
 
     private void OnCollisionEnter2D(Collision2D collision) {
@@ -24,16 +25,13 @@ public class CharacterCollisionDirectionView : View, ICharacterCollisionDirectio
     }
 
     private void OnCollisionExit2D(Collision2D collision) {
-        collisions.Remove(collision);
+        collisions.Remove(collision.collider);
         UpdateCollisionDirection(collision, false);
-    }
-
-    private void LateUpdate() {
-        collisions.Clear();
     }
 
     private void UpdateCollisionDirection(Collision2D collision, bool possiblyDispatchCollisionEvent = true) {
         Vector2 newCollisionDirection = GetCollisionDirection();
+
         if (collisionDirection != newCollisionDirection) {
             collisionDirection = newCollisionDirection;
             if(possiblyDispatchCollisionEvent) {
@@ -43,12 +41,7 @@ public class CharacterCollisionDirectionView : View, ICharacterCollisionDirectio
     }
 
     private Vector2 GetCollisionDirection() {
-        Vector2 combinedCollisionDirection = new Vector2();
-
-        foreach (Vector2 collisionDirection in collisions.Values.ToList()) {
-            combinedCollisionDirection += collisionDirection;
-        }
-
+        Vector2 combinedCollisionDirection = collisions.Values.ToList().CombineVectors();
         combinedCollisionDirection = VectorHelper.Clamp(combinedCollisionDirection, -1, 1);
         return combinedCollisionDirection;
     }
@@ -68,7 +61,11 @@ public class CharacterCollisionDirectionView : View, ICharacterCollisionDirectio
 
         Vector2 combinedDirection = VectorHelper.Clamp(allDirections, -1, 1);
 
-        collisions.Add(collision, combinedDirection);
+        if(collisions.ContainsKey(collision.collider)) {
+            collisions[collision.collider] = combinedDirection;
+        } else {
+            collisions.Add(collision.collider, combinedDirection);
+        }
     }
 
     public void ResetCollisions() {
