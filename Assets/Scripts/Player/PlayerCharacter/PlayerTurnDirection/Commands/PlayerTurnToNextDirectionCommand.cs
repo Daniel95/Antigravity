54f6d6ae17d5c7faf5986fda42db80c570095c2b
Daniel95 <idaniel95@me.com>
2017-11-02 23:35:05 +0100

    make use of velocity direction when not moving standard

--- Assets/Scripts/Player/PlayerCharacter/PlayerTurnDirection/Commands/PlayerTurnToNextDirectionCommand.cs
+++ Assets/Scripts/Player/PlayerCharacter/PlayerTurnDirection/Commands/PlayerTurnToNextDirectionCommand.cs
@@ -9,7 +9,13 @@ public class PlayerTurnToNextDirectionCommand : Command {
     [Inject(Label.Player)] private Ref<ICharacterCollisionDirection> playerCollisionDirectionRef;
 
     protected override void Execute() {
-        Vector2 moveDirection = playerVelocityRef.Get().MoveDirection;
+        Vector2 moveDirection;
+        if (playerVelocityRef.Get().IsMovingStandard) {
+            moveDirection = playerVelocityRef.Get().MoveDirection;
+        } else {
+            moveDirection = playerVelocityRef.Get().GetPreviousVelocityDirection();
+        }
+
         Vector2 collisionDirection = playerCollisionDirectionRef.Get().CollisionDirection;
         RaycastData combinedRaycastData = playerRaycastDirectionRef.Get().GetCombinedDirectionAndCenterDistances();
         Vector2 surroundingsDirection = SurroundingDirectionHelper.GetSurroundingsDirection(collisionDirection, combinedRaycastData.Direction);
